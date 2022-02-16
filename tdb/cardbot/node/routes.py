a6736a18a8e10cce7118e56bf9560259b634d464
Troy Black <troydblack@gmail.com>
2022-02-15 19:45:28 -0600

    Update GPIO Stepper Class

--- tdb/cardbot/node/routes.py
+++ tdb/cardbot/node/routes.py
@@ -8,25 +8,25 @@ class Routes(BaseRoutes):
     router = APIRouter()
 
     @staticmethod
-    @router.get('/head/{direction}/{steps}')
-    def move_head(direction, steps: int):
-        gpio.Gpio.move_head(direction, steps)
+    @router.get('/z/{steps}')
+    def move_head(steps: int):
+        gpio.Gpio.z_stepper.step(steps)
         return True
 
     @staticmethod
-    @router.get('/x/{direction}/{steps}')
-    def move_x(direction, steps: int):
-        gpio.Gpio.move_x(direction, steps)
+    @router.get('/x/{steps}')
+    def move_x(steps: int):
+        gpio.Gpio.x_stepper.step(steps)
         return True
 
     @staticmethod
-    @router.get('/new/{direction}/{steps}')
-    def move_new(direction, steps: int):
-        gpio.Gpio.move_new_lift(direction, steps)
+    @router.get('/input/{steps}')
+    def move_new(steps: int):
+        gpio.Gpio.input_stepper.step(steps)
         return True
 
     @staticmethod
-    @router.get('/sorted/{direction}/{steps}')
-    def move_sorted(direction, steps: int):
-        gpio.Gpio.move_sorted_lift(direction, steps)
+    @router.get('/output/{steps}')
+    def move_sorted(steps: int):
+        gpio.Gpio.output_stepper.step(steps)
         return True
